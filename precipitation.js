3e8bd60e6fa27466d3453d23e790735e2187dbe8
msigot2018 <56771584+msigot2018@users.noreply.github.com>
2020-01-20 15:34:20 +0000

    precipitation
    CHIRPS Daily: Climate Hazards Group InfraRed Precipitation with Station Data (version 2.0 final)

---
+++ precipitation.js
@@ -0,0 +1,29 @@
+//PRECIPITATION IN mm BY DAY FOR 50ºN AND 50ºS TO SPECIFIC AREA 
+// SPATIAL RESOLUTION: ABOUT 5KM 
+//SOURCE: https://chc.ucsb.edu/data/chirps
+
+
+var polygon =  //study area 
+var startDateEarly = ee.Date('start date_yy_mm_dd');
+var endDateEarly = ee.Date('end_date_yy_mm_dd');
+
+//------------------------------------------------------------------------------------------------
+
+var chirpsColl = ee.ImageCollection('UCSB-CHG/CHIRPS/DAILY')
+                .filterDate(startDateEarly, endDateEarly)
+                .filterBounds(polygon);
+
+
+
+print(ui.Chart.image.series(chirpsColl, polygon, ee.Reducer.median(), 1)
+      .setChartType('ScatterChart')
+      .setOptions({
+        title: 'Precipitação',
+        vAxis: {title: ''},
+        hAxis: {title: 'Meses', format: 'MM-yy', gridlines: {count: 20}},
+        lineWidth: 1,
+        pointSize: 0,
+        series: {
+          0: {color: '3B7A57'},}
+      }));
+
